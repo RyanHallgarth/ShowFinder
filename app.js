fbc4cb380c9c87de13aaaee465b5f3d088a11c26
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-06-25 23:45:30 -0400

    deploy troubleshooting

--- app.js
+++ app.js
@@ -1,78 +1,70 @@
+const baseURL = "https://api.tvmaze.com/search/shows";
+const showContainer = document.querySelector("#showContainer");
 
-const baseURL = 'http://api.tvmaze.com/search/shows'
-const showContainer = document.querySelector('#showContainer');
+const form = document.querySelector("#searchForm");
 
-const form = document.querySelector('#searchForm');
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  deleteImages();
+  const userInput = form.elements.query.value;
+  const config = { params: { q: userInput } };
+  const response = await axios.get(`${baseURL}`, config);
 
+  displayImages(response.data);
+  form.elements.query.value = "";
 
+  const st = document.querySelector("#showContainer");
 
-form.addEventListener('submit', async function (event) {
-
-    event.preventDefault();
-    deleteImages();
-    const userInput = form.elements.query.value;
-    const config = { params: { q: userInput } }
-    const response = await axios.get(`${baseURL}`, config);
-
-    displayImages(response.data);
-    form.elements.query.value = "";
-
-    const st = document.querySelector('#showContainer');
-
-    st.style.display = 'flex';
-
-
+  st.style.display = "flex";
 });
 
 const displayImages = (shows) => {
-    let i = 0;
-    for (let result of shows) {
-        if (result.show.image) {
-            const btn = document.createElement('button');
-            btn.setAttribute('data-toggle', 'modal');
-            btn.setAttribute('data-target', '#exampleModal');
+  let i = 0;
+  for (let result of shows) {
+    if (result.show.image) {
+      const btn = document.createElement("button");
+      btn.setAttribute("data-toggle", "modal");
+      btn.setAttribute("data-target", "#exampleModal");
 
-            const img = document.createElement('img');
-            img.src = result.show.image.medium;
-            const title = document.createElement('span');
-            let summary = document.createElement('p');
-            title.textContent = result.show.name;
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      const title = document.createElement("span");
+      let summary = document.createElement("p");
+      title.textContent = result.show.name;
 
-
-
-            let = summaryText = result.show.summary.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, '');
-            summary.textContent = summaryText;
-            btn.append(title);
-            btn.append(img);
-            btn.append(summary);
-            showContainer.append(btn);
-
-        }
-
+      let = summaryText = result.show.summary.replace(
+        /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+        ""
+      );
+      summary.textContent = summaryText;
+      btn.append(title);
+      btn.append(img);
+      btn.append(summary);
+      showContainer.append(btn);
     }
-    showContainer.addEventListener('click', function (event) {
-        const tHolder = document.querySelector('#exampleModalLabel')
-        tHolder.innerText = event.target.previousSibling.innerText;
+  }
+  showContainer.addEventListener("click", function (event) {
+    const tHolder = document.querySelector("#exampleModalLabel");
+    tHolder.innerText = event.target.previousSibling.innerText;
 
-        let sHolder = document.querySelector('#body');
+    let sHolder = document.querySelector("#body");
 
-        sHolder.innerText = event.target.nextSibling.innerText;
+    sHolder.innerText = event.target.nextSibling.innerText;
 
-        const google = document.querySelector('#google')
-        google.setAttribute('href', `https://google.com/search?q=${tHolder.innerText}`)
-    })
-
-}
-
-
-
+    const google = document.querySelector("#google");
+    google.setAttribute(
+      "href",
+      `https://google.com/search?q=${tHolder.innerText}`
+    );
+  });
+};
 
 const deleteImages = () => {
-    const images = document.querySelectorAll('img');
+  const images = document.querySelectorAll("img");
 
-    for (let image of images) {
-        image.remove();
-    }
-}
+  for (let image of images) {
+    image.remove();
+  }
+};
 
 deleteImages();
